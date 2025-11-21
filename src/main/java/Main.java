138035cd8efe0709a961f93e94d0e787b7e06fda
AbdelhamidRouatbi <abdelhamid.rouatbi00@gmail.com>
2025-11-21 10:40:46 -0500

    secret

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String maVariable = System.getenv("PATHAPI");
+        String variable2 = System.getenv("MAVARIABLE");
+        String secret = System.getenv("SECRET");
         System.out.println(maVariable);
+        System.out.println(variable2);
+        System.out.println(secret);
     }
 }
