73734e05b7fec91eb4390136f5884a22f645135a
AbdelhamidRouatbi <abdelhamid.rouatbi00@gmail.com>
2025-11-21 10:29:38 -0500

    env variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String maVariable = System.getenv("PATHAPI");
+        System.out.println(maVariable);
     }
 }
