81d8c6b1523f154b91f269121d2c32e583ebc075
AbdelhamidRouatbi <abdelhamid.rouatbi00@gmail.com>
2025-11-21 10:54:03 -0500

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,5 @@ public class Main {
         String maVariable = System.getenv("PATHAPI");
         String variable2 = System.getenv("MAVARIABLE");
         String secret = System.getenv("SECRET");
-        System.out.println(maVariable);
-        System.out.println(variable2);
-        System.out.println(secret);
     }
 }
